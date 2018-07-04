3c76e9211974d8071521899ad9f3d2ac16e2897f
jakob <jakob.klemm@yahoo.com>
2018-07-04 14:29:27 -0400

    Beginning commit to set up app activities and backgrounds.

--- app/src/main/java/com/example/jakob/ninjagame/MainActivity.java
+++ app/src/main/java/com/example/jakob/ninjagame/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.jakob.ninjagame;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void buttonClick(View button){
+        Button b = (Button)button;
+        String textOfButton = b.getText().toString();
+        Log.i("Button clicked: " , textOfButton);
+    }
+
+    public void changeToPlayScreen(View button) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        Button b = (Button)button;
+        String message = b.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
 }
