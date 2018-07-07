e58272ca39e1c0b296baa9d6f2e0dd582244ff70
jakob <jakob.klemm@yahoo.com>
2018-07-06 21:12:07 -0400

    Add scrolling feature

--- app/src/main/java/com/example/jakob/ninjagame/InGameActvity.java
+++ app/src/main/java/com/example/jakob/ninjagame/InGameActvity.java
@@ -1,7 +1,11 @@
 package com.example.jakob.ninjagame;
 
+import android.animation.ValueAnimator;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.LinearInterpolator;
+import android.widget.ImageView;
 
 public class InGameActvity extends AppCompatActivity {
 
@@ -9,5 +13,24 @@ public class InGameActvity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_in_game_actvity);
+        ConstraintLayout layout = (ConstraintLayout)findViewById(R.id.parentLayout);
+        final ImageView backgroundOne = (ImageView) findViewById(R.id.background_one);
+        final ImageView backgroundTwo = (ImageView) findViewById(R.id.background_two);
+
+        final ValueAnimator animator = ValueAnimator.ofFloat(0.0f, 1.0f);
+        animator.setRepeatCount(ValueAnimator.INFINITE);
+        animator.setInterpolator(new LinearInterpolator());
+        animator.setDuration(5000L);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                final float progress = (float) animation.getAnimatedValue();
+                final float height = backgroundOne.getHeight();
+                final float translationY = height * progress;
+                backgroundOne.setTranslationY(translationY);
+                backgroundTwo.setTranslationY(translationY - height);
+            }
+        });
+        animator.start();
     }
 }
--- app/src/main/java/com/example/jakob/ninjagame/MainActivity.java
+++ app/src/main/java/com/example/jakob/ninjagame/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.jakob.ninjagame;
 
+import android.animation.ValueAnimator;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.LinearInterpolator;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
